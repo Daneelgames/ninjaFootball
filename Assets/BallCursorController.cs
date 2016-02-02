cee7594774f3bf6d796ebeaba7d237112025f1e0
daneel <rubberandrobber@gmail.com>
2016-02-02 23:00:10 +0300

    added ball cursor script [NOT WORKING]

---
+++ Assets/BallCursorController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallCursorController : MonoBehaviour
+{
+
+    public float radius = 2;
+    public float angle = 0;
+
+    public float targetAngle = 0;
+
+
+    public float speed = 10;
+    float velocity = 0;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+
+        if (Input.anyKey)
+        {
+            targetAngle = Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+
+            var shortest = Mathf.DeltaAngle(angle * Mathf.Rad2Deg, targetAngle * Mathf.Rad2Deg) * Mathf.Deg2Rad;
+            targetAngle = angle + shortest;
+        }
+
+
+        angle = Mathf.SmoothDampAngle(angle, targetAngle, ref velocity, 0.01f, 20f);
+
+        transform.position = new Vector3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0);
+    }
+}
