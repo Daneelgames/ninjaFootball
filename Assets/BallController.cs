568d9a4660f708c2a77ac82d099f06441f587436
Daniil Ermakov <d.ermakov@corpwebgames.com>
2016-02-03 15:33:06 +0300

    ball shooting

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,12 +6,15 @@ public class BallController : MonoBehaviour {
 	public GameObject playerPref;
 	public bool inControl = false;
 	public float shootAngle;
+	public float shootPower;
 
 	private GameObject playerGO;
 	private CircleCollider2D cCollider;
 	private GameObject ballCursor;
 	private Rigidbody2D rb;
 	private bool canSpawn = true;
+	private float lastInputX = 1;
+	private float lastInputY = 1;
 
 	void Start() {
 		ballCursor = GameObject.Find("BallCursor");
@@ -26,7 +29,9 @@ public class BallController : MonoBehaviour {
 			transform.position = new Vector2(playerGO.transform.position.x, playerGO.transform.position.y-0.25f);
 			if ((Input.GetAxisRaw("Vertical") != 0) || (Input.GetAxisRaw("Horizontal") != 0) )
 				{
-					shootAngle = Mathf.Atan2 (Input.GetAxis ("Vertical"),Input.GetAxis ("Horizontal")) * Mathf.Rad2Deg;
+					lastInputX = Input.GetAxisRaw("Horizontal");
+					lastInputY = Input.GetAxisRaw("Horizontal");
+					shootAngle = Mathf.Atan2 (Input.GetAxisRaw ("Vertical"),Input.GetAxisRaw ("Horizontal")) * Mathf.Rad2Deg;
 					transform.rotation = Quaternion.Euler(0f, 0f, shootAngle);
 				}
 			if (Input.GetButtonDown ("Fire1"))
@@ -55,7 +60,7 @@ public class BallController : MonoBehaviour {
 		playerGO.GetComponent<PlayerController>().Hide();
 		inControl = false;
 		cCollider.enabled = true;
-		rb.AddForce(new Vector2(Input.GetAxisRaw ("Horizontal"),Input.GetAxisRaw ("Vertical")) * 25f, ForceMode2D.Impulse);
+		rb.AddForce(new Vector2(lastInputX,lastInputY).normalized * shootPower, ForceMode2D.Impulse);
 		canSpawn = true;
 	}
 }
