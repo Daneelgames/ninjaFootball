6d1337dd57a4dfe39878764550a1d9e94e97075a
daneel <rubberandrobber@gmail.com>
2016-02-13 23:55:40 +0300

    art 1st attempt

---
+++ Assets/Scripts/AnimateOffset.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateOffset : MonoBehaviour {
+
+    [ReadOnly]
+    public Material material;
+    public float scrollSpeed;
+
+    private float offset;
+
+	// Use this for initialization
+	void Start () {
+        material = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        offset += scrollSpeed * Time.deltaTime;
+        material.mainTextureOffset = new Vector2(0, offset);
+
+        if (offset >= 10)
+            offset = -10;
+	}
+}
