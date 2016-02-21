a5b090d2ef2da1dec5c7c2828ede11f531c4cacb
daneel <rubberandrobber@gmail.com>
2016-02-21 19:18:17 +0300

    disable explosion collider on time

---
+++ Assets/Scripts/DisableColliderOnTime.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableColliderOnTime : MonoBehaviour {
+
+    [SerializeField]
+    private float time;
+    private Collider2D _collider;
+
+	// Use this for initialization
+	void Start () {
+        _collider = GetComponent<Collider2D>();
+
+        StartCoroutine(DisableCollider());
+	}
+
+    IEnumerator DisableCollider()
+    {
+        yield return new WaitForSeconds(time);
+        _collider.enabled = false;
+    }
+}
--- Assets/Scripts/EnemySystem/EnemyMoveInRange.cs
+++ Assets/Scripts/EnemySystem/EnemyMoveInRange.cs
@@ -8,13 +8,20 @@ public class EnemyMoveInRange : MonoBehaviour {
     [SerializeField]
     private float hDir = 1;
 
+    private GameObject sprite;
+
+    void Start()
+    {
+        sprite = transform.Find("Sprite").gameObject;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (hDir > 0)
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
-        else 
-            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        if (speed > 0)
+            sprite.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        else
+            sprite.transform.localRotation = Quaternion.Euler(0, 180, 0);
         // hDir не меняет направление движения, хз почему
         transform.Translate(hDir * speed * Time.deltaTime, 0, 0);
     }
