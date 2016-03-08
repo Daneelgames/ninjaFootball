b118f4253e7eeec01bed63b81308dc81827e0508
daneel <rubberandrobber@gmail.com>
2016-03-08 18:34:56 +0300

    Librrary

---
+++ Assets/Scripts/ChangeSpriteEverySec.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSpriteEverySec : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite[] sprites;
+    [SerializeField]
+    private SpriteRenderer _renderer;
+    [SerializeField]
+    private float tPeriod;
+
+    
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Zone")
+        {
+            print("Invoke");
+            InvokeRepeating("ChangeSprite", tPeriod, tPeriod);
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D coll)
+    {
+        if (coll.tag == "Zone")
+            CancelInvoke("ChangeSprite");
+    }
+
+    void ChangeSprite()
+    {
+        int newSprite = Random.Range(0, sprites.Length);
+        _renderer.sprite = sprites[newSprite];
+        print(newSprite);
+    }
+}
---
+++ Assets/Scripts/DoorLogic.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorLogic : MonoBehaviour {
+
+    [SerializeField]
+    private Transform destination;
+    [SerializeField]
+    private AudioSource _audio;
+
+    private Animator canvasAnimator;
+    private bool inTrigger = false;
+    private bool isInDialog = false;
+    private GameObject player;
+    private PlayerMovement playerScript;
+
+
+    // Use this for initialization
+    void Start () {
+        canvasAnimator = GameObject.Find("Canvas").GetComponent<Animator>();
+        player = GameObject.Find("Player");
+        playerScript = player.GetComponent<PlayerMovement>() as PlayerMovement;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (inTrigger)
+        {
+            if (!isInDialog && Input.GetAxisRaw("Vertical") > 0 && playerScript.isOnGround)
+            {
+                isInDialog = true;
+                StartCoroutine(Teleport());
+            }
+        }
+    }
+
+    IEnumerator Teleport()
+    {
+        canvasAnimator.SetTrigger("Dead");
+        Physics2D.IgnoreLayerCollision(10, 12, ignore: true);
+        Physics2D.IgnoreLayerCollision(10, 11, ignore: true);
+        Physics2D.IgnoreLayerCollision(10, 16, ignore: true);
+        Physics2D.IgnoreLayerCollision(10, 17, ignore: true);
+        yield return new WaitForSeconds(.25F);
+        _audio.Play();
+        _audio.pitch = Random.Range(0.75f, 1.25f);
+        player.transform.position = destination.transform.position;
+        yield return new WaitForSeconds(.75F);
+        Physics2D.IgnoreLayerCollision(10, 12, ignore: false);
+        Physics2D.IgnoreLayerCollision(10, 11, ignore: false);
+        Physics2D.IgnoreLayerCollision(10, 16, ignore: false);
+        Physics2D.IgnoreLayerCollision(10, 17, ignore: false);
+        isInDialog = false;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            inTrigger = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            inTrigger = false;
+        }
+    }
+}
