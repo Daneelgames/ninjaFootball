2fe2761fad3682671a461ed25a636922bd8a4e2c
Daniil Ermakov <rubberandrobber@gmail.com>
2016-03-29 17:47:35 +0300

    savingSystem_0

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SaveManager : MonoBehaviour {
+
+    public Dictionary<GameObject, Vector3> objectsOnScene;
+
+    private GameObject player;
+    private Vector3 playerPos;
+    private int playerAmmo;
+    private GameObject playerExtraWeapon;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    public void Save()
+    {
+        playerPos = player.transform.position;
+        playerAmmo = player.GetComponent<Weapon>().altWeaponAmmo;
+        playerExtraWeapon = player.GetComponent<Weapon>().altWeapon;
+
+
+    }
+
+    void Load()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Statefull.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Statefull : MonoBehaviour {
+    
+    private SaveManager saveManager;
+
+    void Start ()
+    {
+        saveManager = GameObject.Find("SceneManager").GetComponent<SaveManager>();
+
+        SetGameObjectToDictionary();
+    }
+
+    void  SetGameObjectToDictionary()
+    {
+        saveManager.objectsOnScene.Add(gameObject, transform.position);
+    }
+
+    void OnDestroy()
+    {
+        saveManager.objectsOnScene.Remove(gameObject);
+    }
+}
