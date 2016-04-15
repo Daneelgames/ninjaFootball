cc00da17a37439bca73e1f9d2acaff564056813d
Daniil Ermakov <rubberandrobber@gmail.com>
2016-04-15 11:24:01 +0300

    SpikesSwingTrap pt1

---
+++ Assets/Scripts/EnemySystem/TrapButtonController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapButtonController : MonoBehaviour {
+
+    [SerializeField]
+    private Animator trapAnimator;
+    [SerializeField]
+    private Animator buttonAnimator;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Player" || coll.tag == "Enemy")
+        {
+            buttonAnimator.SetTrigger("Action");
+            trapAnimator.SetTrigger("Action");
+        }
+    }
+}
