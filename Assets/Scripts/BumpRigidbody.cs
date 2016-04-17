a22308eabd76fafc2b635f6d28d213494f753806
daneel <rubberandrobber@gmail.com>
2016-04-17 21:59:15 +0300

    new assets structure

---
+++ Assets/Scripts/BumpRigidbody.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BumpRigidbody : MonoBehaviour {
+
+    [SerializeField]
+    private Animator _animator;
+    [SerializeField]
+    private AudioSource _audio;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Player" || coll.tag == "Enemy")
+        {
+            Rigidbody2D rigid = coll.GetComponent<Rigidbody2D>() as Rigidbody2D;
+            rigid.AddForce(Vector2.up * 1000, ForceMode2D.Force);
+            _animator.SetTrigger("Action");
+            _audio.Play();
+            _audio.pitch = Random.Range(.75f, 1.25f);
+        }
+    }
+}
